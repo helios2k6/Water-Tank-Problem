9ee6274d51006e7bfd58adfab1e56333afdf2da0
Andrew Johnson <helios2k6@gmail.com>
2014-01-23 04:16:22 +0000

    removed driver file since it was pointless. Added a move concept so that we can start representing the actual solution

---
+++ Water Tank Simulation/Move.fs
@@ -0,0 +1,12 @@
+﻿namespace WaterTank
+
+namespace Solver
+   open MathNet.Numerics
+
+   (* 
+    * Represents one move between two water tanks. Does not reference the water tanks to avoid
+    * ambiguitity between the "new" water tanks and the "old" water tanks
+    *)
+   type Move = 
+      { Start : uint64; End : uint64; Amount : BigRational }
+      override this.ToString() = sprintf "Move from %A to %A with %A amount of water transfered" this.Start this.End this.Amount
